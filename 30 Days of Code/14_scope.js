c57a9272266c8d5cf3383dcc37d1a15df6e852f4
Noah Henry <nhenryproductions@gmail.com>
2020-04-22 16:10:22 -0500

    day 14 problem solution in JavaScript, even though hackerrank.com does not support it for this problem...

--- 30 Days of Code/14_scope.js
+++ 30 Days of Code/14_scope.js
@@ -1,2 +1,44 @@
-// hackerrank.com does not support JavaScript for this problem.
+// hackerrank.com does not support JavaScript for this problem...
 // todo: Give it a go in Swift?
+
+/*
+Complete the Difference class by writing the following:
+  A class constructor that takes an array of integers as a parameter and saves it to the 'elements' instance variable.
+  A computeDifference method that finds the maximum absolute difference between any '2' numbers in 'N' and stores it in the 'maximumDifference' instance variable.
+*/
+
+// JavaScript Solution (if hackerrank supported it for this problem):
+class Difference {
+  constructor(elements) {
+    this.elements = elements
+    this.maximumDifference = null
+  }
+
+  computDifference() {
+    for (let i = 0; i < this.elements.length; i++) {
+      let base = this.elements[i]
+      
+      for (let j = i + 1; j < this.elements.length; j++) {
+        let comparar = this.elements[j]
+        let getDiff = function(a, b) { return Math.abs(a - b) }
+        let diff = getDiff(base, comparar)
+
+        if (diff > this.maximumDifference) {
+          this.maximumDifference = diff
+        }
+      }
+    }
+  }
+}
+
+function solution(arr) {
+  // const n = 3
+  const a = arr
+  
+  const d = new Difference(a)
+  d.computDifference()
+
+  console.log(d.maximumDifference)
+}
+solution([1,2,5])
+solution([4,6,12])
